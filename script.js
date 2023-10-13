3ca13d54df1aa599441e143b36a8ac31f7b9965b
jeremias3025 <jeremias3025@gmail.com>
2023-10-13 17:27:33 -0300

    Foi add a função convertValue() no Final do codigo

--- script.js
+++ script.js
@@ -56,8 +56,9 @@ function ChangeCurrency() {
         Paragrafo.innerHTML = 'Franco Suiço'
         Flag.src = './assets/Franco suiço.jpg'
     }
-}
 
+    ConvertValue()
+}
 
 CurrencySelect.addEventListener('change', ChangeCurrency)
 buttonToConvert.addEventListener("click", ConvertValue)
